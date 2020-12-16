2071b586a543a8b2f59cc88a0de831c451a416fa
Alie Stone <helloaliestone@gmail.com>
2020-12-16 14:32:43 +0000

    Automatically backed up by Learn

---
+++ lib/waiter.rb
@@ -0,0 +1,23 @@
+class Waiter
+
+  attr_accessor :name, :yrs_experience
+
+  @@all = []
+
+  def new (name, yrs_experience)
+    @name = name
+    @yrs_experience = years
+    @@all << self
+  end
+
+  def new_meal(customer, total, tip=0)
+   Meal.new(self, customer, total, tip)
+ end
+
+ def meals
+   Meal.all.select do |meal|
+   meal.waiter == self
+   end
+ end
+
+end
