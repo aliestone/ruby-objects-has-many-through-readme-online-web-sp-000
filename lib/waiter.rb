7bef3f9e60370c9b8d81f46820ddfc7d02770d8b
Alie Stone <helloaliestone@gmail.com>
2020-12-16 14:46:14 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -15,7 +15,7 @@ class Waiter
   end
 
   def new_meal(customer, total, tip=0)
-    Meal.new (self,customer,total, tip)
+    Meal.new (self,customer,total,tip=0)
   end
 
   def meals
@@ -31,4 +31,4 @@ class Waiter
   best_tipped_meal.customer
   end
 end
-end 
+end
